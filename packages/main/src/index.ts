c3cf39a17a0148a1e96528202c2583ce78c6e23a
Tyler Natsuhara <ty@ty.pizza>
2023-04-01 17:46:22 -0700

    remove vue devtools code

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -35,28 +35,6 @@ app.whenReady()
     .catch((e) => console.error("Failed create window:", e))
 
 /**
- * Install Vue.js or any other extension in development mode only.
- * Note: You must install `electron-devtools-installer` manually
- */
-// if (import.meta.env.DEV) {
-//   app
-//     .whenReady()
-//     .then(() => import('electron-devtools-installer'))
-//     .then(module => {
-//       const {default: installExtension, VUEJS3_DEVTOOLS} =
-//         // @ts-expect-error Hotfix for https://github.com/cawa-93/vite-electron-builder/issues/915
-//         typeof module.default === 'function' ? module : (module.default as typeof module);
-//
-//       return installExtension(VUEJS3_DEVTOOLS, {
-//         loadExtensionOptions: {
-//           allowFileAccess: true,
-//         },
-//       });
-//     })
-//     .catch(e => console.error('Failed install extension:', e));
-// }
-
-/**
  * Check for app updates, install it in background and notify user that new version was installed.
  * No reason run this in non-production build.
  * @see https://www.electron.build/auto-update.html#quick-setup-guide
